c24da26172a65ca9f775a7cbf1acf82fa654198a
Sisi Cheng <scheng@hmc.edu>
2014-11-02 12:52:26 -0800

    Added brainstorming ideas

--- algsmeister/src/test/scala/algsmeister/parser/ParserTest.scala
+++ algsmeister/src/test/scala/algsmeister/parser/ParserTest.scala
@@ -1,11 +1,11 @@
-package algsmeister.parser
-
-import org.scalatest._
-
-import algsmeister.ir._
-import algsmeister.parser._
-import edu.hmc.langtools._
-
-class ParserTests extends FunSpec with LangParseMatchers[AST] {
-  // TODO
-}
+//package algsmeister.parser
+//
+//import org.scalatest._
+//
+//import algsmeister.ir._
+//import algsmeister.parser._
+//import edu.hmc.langtools._
+//
+//class ParserTests extends FunSpec with LangParseMatchers[AST] {
+//  // TODO
+//}
--- algsmeister/src/test/scala/algsmeister/semantics/SemanticsTest.scala
+++ algsmeister/src/test/scala/algsmeister/semantics/SemanticsTest.scala
@@ -1,13 +1,13 @@
-package algsmeister.semantics
-
-import org.scalatest._
-
-import algsmeister.ir._
-import algsmeister.parser._
-import algsmeister.semantics._
-import edu.hmc.langtools._
-
-class SemanticsTests extends FunSpec
-    with LangInterpretMatchers[AST, Int] {
-  // TODO
-}
+//package algsmeister.semantics
+//
+//import org.scalatest._
+//
+//import algsmeister.ir._
+//import algsmeister.parser._
+//import algsmeister.semantics._
+//import edu.hmc.langtools._
+//
+//class SemanticsTests extends FunSpec
+//    with LangInterpretMatchers[AST, Int] {
+//  // TODO
+//}
